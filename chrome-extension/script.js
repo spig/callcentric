fc0683dad705e508315fd1377897f1a932a0ec55
Steve Spigarelli <spigarellisj@ldschurch.org>
2014-03-06 17:36:37 -0700

    updated script.js to reflect some new things I've learned about var and such

--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -2,18 +2,26 @@
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://my.callcentric.com/phone_book.php", true);
+
 xhr.onload = function() {
-  phoneNumbers = xhr.responseText.match(/[0-9]{11}/gm);
-  var uniqPhoneNumbers = {};
+  var phoneNumbers = xhr.responseText.match(/[0-9]{11}/gm),
+      uniqPhoneNumbers = {};
+
   phoneNumbers.forEach(function(val) { uniqPhoneNumbers[val] = val; });
+
   Object.keys(uniqPhoneNumbers).forEach(function(phoneNumber) {
-    var nodes = document.querySelectorAll("a[href*='dial_number1="+phoneNumber+"']")
-    for (var i=0; i<nodes.length; i++) {
-      var item = nodes[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
+    var nodes,
+        i = 0,
+        item;
+    nodes = document.querySelectorAll("a[href*='dial_number1="+phoneNumber+"']")
+    for (i=nodes.length; i--;) {
+      item = nodes[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
       item.classList.add("inPhonebook");
     }
+    // $("a[href*='dial_number1="+phoneNumber+"']").addClass("inPhonebook");
   });
 };
+
 // call before send
 xhr.overrideMimeType("text/xml");
 xhr.send();
